63cd9277116bc6bbaf84a602b81e1c62e3557c52
ChilliGuy <102238523+ginsengcandy@users.noreply.github.com>
2026-02-20 13:24:08 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 6.85 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,38 +1,33 @@
-import java.math.BigInteger;
+/*
+Objective: compare values in the string while iterating
+
+Strategy: convert values to Long, instead of BigInteger
+
+Ground: the length of p is less than 18 -> when converted to number type, it can be covered by long types(type long can handle up to 20 digits of decimal number)
+
+Approach:
+1) convert p to long -> to compare against its counterparts derived from t
+2) convert p's counterpart from t
+3) compare converted p and its converted counterpart
+4) if p is bigger, add value to answer
+5) reassign p's counterpart by moving the iterator until its end reaches the end index of t
+6) start over step 2 with reassigned iterator
+*/
 
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-        final int iteration = t.length() - p.length() + 1;
-
-        for(int i = 0;i < iteration; i++){
-            String tsub = t.substring(i, i+p.length());
-            
-            BigInteger tBigInt = new BigInteger(tsub);
-            BigInteger pBigInt = new BigInteger(p);
-            
-            int isBigger = tBigInt.compareTo(pBigInt);
-            
-            if(isBigger < 1) {
+        int pLength = p.length();
+        long pValue = Long.parseLong(p);
+        
+        for(int i = 0;i <= t.length() - pLength; i++) {
+            long tValue = Long.parseLong(t.substring(i, i+pLength));
+            if(tValue <= pValue) {
                 answer++;
-            } 
+            }
         }
         return answer;
     }
 }
 
-/*
-시간복잡도: O(n): t의 길이만큼 순회하므로
-length(t) = 5
-length(p) = 3
-5-3+1만큼 순회(3회)
-t = "20139"
-p = "101"
-201 013 139
-101 101 101
-숫자열 간 크기 비교 2가지 -> parseInt(숫자로 변환), BigInteger.compareTo(숫자가 클 때)
-p의 길이가 최대 18(10의 17승)이므로 BigInteger로 변환한 뒤 비교하는 것이 바람직.
-p의 길이만큼 t의 문자열을 읽는 것이 중요 -> 문자열 자르기 substring(시작 포함, 끝 인덱스 불포함)
-시작을 i라고 하면 끝은 i+p
-(t-p+1)만큼 반복
-*/
+
