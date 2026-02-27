204a4e8c8e074390f399a8b5d7fa2848ec6cec94
ChilliGuy <102238523+ginsengcandy@users.noreply.github.com>
2026-02-27 11:04:23 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,19 +3,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         
-        List<Integer> list = new ArrayList<>();
-        
-        for(int[] command:commands){
-            int start = command[0]-1;
-            int end = command[1];
-            int loc = command[2]-1;
+        int[] answer = new int[commands.length];
+        for(int i = 0; i < commands.length; i++){
+            //nice use of variables
+            int start = commands[i][0]-1; // conversion from 1-based to 0-based index.
+            int end = commands[i][1]; // copyOfRange is open in the end: keep 1-based.
+            int loc = commands[i][2]-1;
+            
             int[] arrayCopy = Arrays.copyOfRange(array, start, end);
             Arrays.sort(arrayCopy);
-            list.add(arrayCopy[loc]);
+            answer[i] = arrayCopy[loc];
         }
         
-        return list.stream()
-            .mapToInt(Integer::intValue)
-            .toArray();
+        return answer;
     }
 }
+
+/*
+Check if indexes are given as 1-based or 0-based.  
+When converting list to arrays of primitive classes, make sure list houses wrapper classes that need to be converted first.
+*/
