7f1e42a0052ecdddf2bc5442fb0c21f261fb88eb
ChilliGuy <102238523+ginsengcandy@users.noreply.github.com>
2026-07-24 12:53:13 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 9.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,58 +1,54 @@
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
         
-        // 파라미터와 지역 변수 이름은 같을 수 없다.
         int videoLen = toSeconds(video_len);
-        int current = toSeconds(pos);
+        int position = toSeconds(pos);
         int opStart = toSeconds(op_start);
         int opEnd = toSeconds(op_end);
         
-        for(String command: commands) {
-            // 오프닝 구간 1차 확인
-            if(current >= opStart && current <= opEnd) {
-                current = opEnd;
+        for (String command : commands) {
+            
+            
+            // 오프닝이 끝나는 위치로 이동해야 하는지 검사
+            if (position >= opStart && position <= opEnd) {
+                position = opEnd;
             }
-            // 이동 명령 수행
-            if(command.equals("prev")) {
-                current = Math.max(0, current-10);
+            // 명령 수행
+            if (command.equals("prev")) {
+                position = Math.max(0, position-10);
+            } else {
+                position = Math.min(videoLen, position+10);
             }
-            if (command.equals("next")) {
-                current = Math.min(videoLen, current+10);
-            }
-            // 오프닝 구간 2차 확인
-            if(current >= opStart && current <= opEnd) {
-                current = opEnd;
+            // 명령 수행 후 오프닝 위치에 있는지 재확인
+            if (position >= opStart && position <= opEnd) {
+                position = opEnd;
             }
         }
         
-        return toTime(current);
- 
-    }
-    // "mm:ss"를 초로 변환
-    private int toSeconds(String time) {
-        int minute = 60 * Integer.parseInt(time.substring(0,2));
-        int seconds = Integer.parseInt(time.substring(3,5));
-        
-        return minute + seconds;
-    }
-    // 초를 "mm:ss"로 변환
-    private String toTime(int time) {
-        int minute = time / 60;
-        int second = time % 60;
+        // 정수 형태의 position을 "mm:ss" 형태로 변환 후 출력
+        int minute = position / 60;
+        int second = position % 60;
         
         return String.format("%02d:%02d", minute, second);
+        
     }
+    // 문자열 -> 정수(초)로 시간을 변환해주는 함수
+    private int toSeconds(String time) {
+        int minute = Integer.parseInt(time.substring(0,2));
+        int second = Integer.parseInt(time.substring(3,5));
+        
+        return minute * 60 + second;
+    }
+    
+    
 }
 /*
-상태 pos
-
-입력 commands
-
+상태
+pos
+입력
+commands - prev, next
 상태 변화
-prev -> max(0, pos-10)
-next -> min(video_len, pos+10)
-
+pos + 10 또는 pos - 10 (초)
 예외
 op_start <= pos <= op_end
--> opEnd
 */
